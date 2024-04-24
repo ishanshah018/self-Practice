3b97b37ab2d2bfa3829bcfb6b225e71eef2de0ef
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-04-25 00:59:53 +0530

    thread_pract13.java
    join() method

--- thread_pract13.java
+++ thread_pract13.java
@@ -7,7 +7,6 @@ class MyThread extends Thread {
     public void run() {
         for (int i = 0; i <= 5; i++) {
             System.out.println("Child Thread");
-            Thread.yield();
         }
     }
 }
