b2a495cc9c2a7f19e1f525f45e048c7126f459c3
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-04-24 21:29:16 +0530

    thread_pract2.java

--- thread_prac2.java
+++ thread_prac2.java
@@ -13,7 +13,7 @@ class MyThread extends Thread {
 class ThreadDemo {
     public static void main(String[] args) {
         MyThread mt1 = new MyThread();
-        mt1.start(); // This method is of Thread class
+        mt1.start();
 
         System.out.println("Main Thread");
     }
